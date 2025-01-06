402942585a7703ddf1c9f391c3ea651eca3d5d83
Jeremias Quinteros <quinterosjeremias8@gmail.com>
2025-01-06 01:56:41 -0300

    cors

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ import cors from 'cors'
 dotenv.config();
 
 const corsOptions = {
-    origin:'http://localhost:5173', // Especifica el origen permitido
+    origin:'https://subjectsmanager.vercel.app/', // Especifica el origen permitido
     methods: 'GET,POST,PUT,DELETE', // Métodos permitidos
     allowedHeaders: 'Content-Type,Authorization', // Encabezados permitidos
   };
